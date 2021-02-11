d668fd9d6adbf7c05e65d2117550e11635146c7b
Juraj Bublinec <jurajbublinec@gmail.com>
2021-02-11 07:43:41 +0100

    feat: connet models to views

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -1,42 +1,72 @@
 import django
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
-
 django.setup()
-from rango.models import Category, Page
 
+from rango.models import Category, Page
 
 
 def populate():
     python_pages = [
-        {'title': 'Official Python Tutorial',
-            'url': 'http://docs.python.org/3/tutorial/'},
-        {'title': 'How to Think like a Computer Scientist',
-            'url': 'http://www.greenteapress.com/thinkpython/'},
-        {'title': 'Learn Python in 10 Minutes',
-            'url': 'http://www.korokithakis.net/tutorials/python/'}
+        {
+            'title': 'Official Python Tutorial',
+            'url': 'http://docs.python.org/3/tutorial/',
+            'views': 2
+        },
+        {
+            'title': 'How to Think like a Computer Scientist',
+            'url': 'http://www.greenteapress.com/thinkpython/',
+            'views': 2
+        },
+        {
+            'title': 'Learn Python in 10 Minutes',
+            'url': 'http://www.korokithakis.net/tutorials/python/',
+            'views': 2
+        }
     ]
     django_pages = [
-        {'title': 'Official Django Tutorial',
-            'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title': 'Django Rocks', 'url': 'http://www.djangorocks.com/'},
-        {'title': 'How to Tango with Django',
-            'url': 'http://www.tangowithdjango.com/'}
+        {
+            'title': 'Official Django Tutorial',
+            'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+            'views': 2
+
+        },
+        {
+            'title': 'Django Rocks', 
+            'url': 'http://www.djangorocks.com/',
+            'views': 2
+         },
+        {
+            'title': 'How to Tango with Django',
+            'url': 'http://www.tangowithdjango.com/',
+            'views': 2
+         }
     ]
     other_pages = [
-        {'title': 'Bottle', 'url': 'http://bottlepy.org/docs/dev/'},
-        {'title': 'Flask', 'url': 'http://flask.pocoo.org'}
+        {
+            'title': 'Bottle',
+            'url': 'http://bottlepy.org/docs/dev/',
+            'views': 2
+         },
+        {
+            'title': 'Flask', 
+            'url': 'http://flask.pocoo.org',
+            'views': 2,
+         }
     ]
+
     cats = {
         'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
         'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
         'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}
     }
+
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data["pages"]:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
