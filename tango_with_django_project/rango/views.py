e68cd63b6b2ed011c6348b0139be4da67a55f25b
Juraj Bublinec <jurajbublinec@gmail.com>
2021-02-12 10:52:07 +0100

    feat: visitors counter usin server-side sessions

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,9 +3,39 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from datetime import datetime
 
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from rango.models import Category, Page
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        print("here")
+        visits += 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+        
+    # Update/set the visits cookie
+    request.session['visits'] = visits
 
 
 def index(request):
@@ -23,16 +53,27 @@ def index(request):
     pages_list = Page.objects.order_by('-views')[:5]
     context_dict = {}
 
+    # Call the helper function to handle the cookies
+    visitor_cookie_handler(request)
+
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = pages_list
-# Render the response and send it back!
-    return render(request, 'rango/index.html', context=context_dict)
+
+    response = render(request, 'rango/index.html', context=context_dict)
+
+    # Render the response and send it back
+    return response
 
 
 def about(request):
     context_dict = {
         'tutorial': 'This tutorial has been put together by Juraj Bublinec.'}
+
+    # Call the helper function to handle the cookies
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    
     return render(request, 'rango/about.html', context=context_dict)
 
 
